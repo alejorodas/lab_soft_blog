2f78c1b65e94be2d574d1d880ab6105e3c705f11
alejorodasvasquez <alejorodasvasquez@gmail.com>
2018-03-18 02:05:52 -0500

    se configuran validaciones del modelo Article #HU-1

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,10 @@
 class Article < ApplicationRecord
+  validates :title, presence: { message: "El titulo del articulo debe estar presente" }, 
+            uniqueness: { message: "El titulo del articulo debe ser unico" },
+            format: { :with => /\A[a-zA-Z]+\z/, :message => "El titulo solo debe tener letras" }
+
+  validates :body, presence: { message: "El cuerpo del articulo debe estar presente" },
+            format: { :with => /\A[a-zA-Z]+\z/, :message => "El cuerpo del articulo solo debe tener letras" }
+
+  validates :published_at, presence: { message: "La fecha del articulo debe estar presente" }
 end
