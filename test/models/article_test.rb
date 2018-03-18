1b57e68057fd5e489e2d2e06f37652e18713e928
alejorodasvasquez <alejorodasvasquez@gmail.com>
2018-03-18 02:20:20 -0500

    se crean los test para aceptación #HU-1

--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -4,4 +4,21 @@ class ArticleTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "article_fields_are_not_present" do
+    article = Article.new()
+    assert_not(article.save, "La entidad se guarda sin campos #{article.errors.messages}")
+  end
+
+  test "article_fields_are_not_correct_type" do
+    article = Article.new(title: 123, body: 1223, published_at: Date.today.to_s)
+    assert_not(article.save, "La entidad guarda con los tipos de los campos incorrectos #{article.errors.messages}")
+  end
+
+  test "article_title_should_be_unique" do
+    article = Article.new(title: "UML", body: "Lorem", published_at: Date.today.to_s)
+    article.save
+    article = Article.new(title: "UML", body: "Lorem", published_at: Date.today.to_s)
+    assert_not(article.save, "La entidad guarda repitiendo nombre del articulo #{article.errors.messages}")
+  end
 end
