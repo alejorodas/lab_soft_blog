3bf44e95ea83244803b87de61ce246938300518c
alejorodasvasquez <alejorodasvasquez@gmail.com>
2018-03-19 09:25:18 -0500

    se crean las validaciones para el modelo Comments #HU-2

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,8 @@
 class Comment < ApplicationRecord
   belongs_to :article
+
+  validates :name, presence: {message: "El nombre de la persona debe estar presente"}
+  validates :email, presence: {message: "El email debe estar presente"}, 
+    format: { with: /\w+@\w+\.{1}[a-zA-Z]{2,}/, message: "Formato incorrecto email" }
+  validates :body, presence: {message: "Debe escribir un comentario"}
 end
